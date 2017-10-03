200fb99183feb5b480d57223f9cbdc46c0c45fc2
lokhiufung <32426749+lokhiufung@users.noreply.github.com>
2017-10-03 23:57:09 +0800

    updated version of two layer model
    I have fixed the stupid problem.

--- 2_layer_network.py
+++ 2_layer_network.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[23]:
+# In[188]:
 
 
 import numpy as np 
 import pandas as pd
 
 
-# In[24]:
+# In[189]:
 
 
 def initialize_parameters(layers_dims):
@@ -27,7 +27,7 @@ def initialize_parameters(layers_dims):
     
 
 
-# In[39]:
+# In[190]:
 
 
 def predict(x, parameters): 
@@ -46,7 +46,7 @@ def predict(x, parameters):
     return A2
 
 
-# In[25]:
+# In[191]:
 
 
 def sigmoid(Z):
@@ -55,7 +55,7 @@ def sigmoid(Z):
     
 
 
-# In[26]:
+# In[192]:
 
 
 def relu(Z):
@@ -63,73 +63,81 @@ def relu(Z):
     return A
 
 
-# In[27]:
+# In[193]:
 
 
 def compute_cost(A2, y):
     
-    cost = np.sum(np.multiply(y, A2) + np.multiply(1-y,1-A2))
+    m = y.shape[1]
+    cost = -np.sum((np.multiply(y, np.log(A2)) + np.multiply(1-y,np.log(1-A2))))/m
     
     return cost
 
 
-# In[31]:
+# In[195]:
 
 
-def two_layer_model(x, y, layers_dims, learning_rate=0.0075, itera=1e-3, print_cost=False):
+def two_layer_model(x, y, layers_dims, learning_rate=0.0075, itera=3000, print_cost=False):
     
-    parameters = initialize_parameters(layers_dims)
+    #parameters = initialize_parameters(layers_dims)
+    n_input, n_hidden, n_output = layers_dims
+    W1 = np.random.randn(n_hidden, n_input)
+    b1 = np.zeros((n_hidden, 1))
+    W2 = np.random.randn(n_output, n_hidden)
+    b2 = np.zeros((n_output, 1))
     
-    W1 = parameters['W1']
-    b1 = parameters['b1']
-    W2 = parameters['W2']
-    b2 = parameters['b2']
-    
+    parameters = {}
     m = y.shape[1]
     costs = []
     
     for i in range(itera):
         
-        Z1 = np.dot(W1, x) + b1
-        A1 = relu(Z1)  #relu 
+        A1 = sigmoid(np.dot(W1, x) + b1)  #relu 
         
-        Z2 = np.dot(W2, A1) + b2
-        A2 = sigmoid(Z2)  #sigmoid
+        A2 = sigmoid(np.dot(W2, A1) + b2)  #sigmoid
         
         cost = compute_cost(A2, y)
         
         #initialize cost
-        dA2 = -np.divide(y, np.log(A2))+np.divide(1-y, np.log(1-A2))
+        #dA2 = -np.divide(y, np.log(A2))+np.divide(1-y, np.log(1-A2))
         dZ2 = A2 - y
         dW2 = np.dot(dZ2, A1.T)/m
         db2 = np.sum(dZ2, axis=1, keepdims= True)/m
         
-        dA1 = np.dot(W2.T, dZ2)
-        dZ1 = dA1 * (Z1>0)
+     
+        dZ1 = np.dot(W2.T, dZ2) * (A1*(1-A1))
         dW1 = np.dot(dZ1, x.T)/m
         db1 = np.sum(dZ1, axis=1, keepdims= True)/m
         
-        W1 = W1 - learning_rate*dW1
-        b1 = b1 - learning_rate*db1
-        W2 = W2 - learning_rate*dW2
-        b2 = b2 - learning_rate*db2
+        W1 = W1 - learning_rate*dW1 #np.dot(dZ1, x.T)/m
+        b1 = b1 - learning_rate*db1 #np.sum(dZ1, axis=1, keepdims= True)/m
+        W2 = W2 - learning_rate*dW2 #np.dot(dZ2, A1.T)/m
+        b2 = b2 - learning_rate*db2 #np.sum(dZ2, axis=1, keepdims= True)/m
+        
         
         if print_cost and i % 100 == 0:
             print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
         if i%100 == 0:
             costs.append(cost)
-            
-        plt.plot(np.squeeze(costs))
+        
+        
+    parameters['W1'] = W1 
+    parameters['b1'] = b1 
+    parameters['W2'] = W2 
+    parameters['b2'] = b2 
+        
+    plt.plot(costs)
+    plt.show()
         
     return parameters, costs    
         
         
 
 
-# In[32]:
-### testing the network ###
+# In[196]:
 
-np.random.seed(1)
+
+
 train_x = np.random.randn(1, 20).reshape(1,20)
 train_y = np.zeros((1, 20))
 train_y[train_x>=0.1] = 1
@@ -139,21 +147,15 @@ m = train_y.shape[1]
 layers_dims = [1,3,1]
 
 
-# In[22]:
-
-
-print (train_x)
-
-
-# In[38]:
+# In[197]:
 
 
 import matplotlib.pyplot as plt
 
-parameters, costs = two_layer_model(train_x, train_y, layers_dims, learning_rate = 1e-8, itera = 3000, print_cost=True)
+parameters, costs = two_layer_model(train_x, train_y, layers_dims, learning_rate=1, itera = 60000, print_cost=False)
 
 
-# In[43]:
+# In[198]:
 
 
 y_prob = predict(train_x, parameters)
@@ -164,7 +166,7 @@ y_pred = np.squeeze(y_pred)
 
 from sklearn.metrics import accuracy_score
 accuracy_score(np.squeeze(train_y), y_pred)
-### accuracy score is very low
+
 
 # In[ ]:
 
